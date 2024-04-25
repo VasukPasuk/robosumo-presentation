cbb43f89ba4307d13041537f6f730c00fc4f8a14
VasukPasuk <vasapaskivskij@gmail.com>
2024-04-25 18:48:07 +0300

    changes

--- src/Components/EndPage/EndPage.jsx
+++ src/Components/EndPage/EndPage.jsx
@@ -26,13 +26,13 @@ function EndPage(props) {
         </div>
         <div style={{width: `50%`, height: '1.25px', background: 'gray'}}/>
         <div className="contact-icons-container">
-          <a href="#">
+          <a href="https://www.google.com/url?q=https%3A%2F%2Ft.me%2Fkrop_robots&sa=D&sntz=1&usg=AOvVaw3vUQ9H8GIBWV7TCiHrGLfF">
             <FaInstagram className="contact-icon"/>
           </a>
-          <a href="#">
+          <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.instagram.com%2Fkrop_robots%2F%3Figsh%3DNXN4d291ZHJ0eng2&sa=D&sntz=1&usg=AOvVaw0SKUxLed1eq-zOiMNcSOr8">
             <FaTelegramPlane className="contact-icon"/>
           </a>
-          <a href="#">
+          <a href="https://www.google.com/url?q=https%3A%2F%2Fwww.facebook.com%2Fgroups%2F1301857734041287&sa=D&sntz=1&usg=AOvVaw1QOR1v6JxQrRJhz9S8lnt_">
             <FaFacebookF className="contact-icon"/>
           </a>
         </div>
